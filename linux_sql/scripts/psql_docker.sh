b477f387235f024775d69129515a4f5f8d8ed83e
Amadou Sy <amadou9550@gmail.com>
2024-12-12 20:01:34 +0000

    Update of psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -5,55 +5,63 @@ cmd=$1
 db_username=$2
 db_password=$3
 
-# Start docker
-# Make sure you understand the double pipe operator
-sudo systemctl status docker || systemctl #todo
+# Start Docker service
+# Use "||" to start Docker if it's not running
+sudo systemctl status docker || sudo systemctl start docker
 
-# Check container status (try the following cmds on terminal)
-docker container inspect jrvs-psql
+# Check container status
+# Inspect the container to see if it exists
+docker container inspect jrvs-psql > /dev/null 2>&1
 container_status=$?
 
-# User switch case to handle create|stop|start opetions
+# Use switch-case to handle create|stop|start options
 case $cmd in 
   create)
-  
-  # Check if the container is already created
-  if [ $container_status -eq 0 ]; then
-		echo 'Container already exists'
-		exit 1	
-	fi
+    # Check if the container already exists
+    if [ $container_status -eq 0 ]; then
+      echo "Container already exists"
+      exit 1
+    fi
 
-  # Check # of CLI arguments
-  if [ $# -ne 3 ]; then
-    echo 'Create requires username and password'
-    exit 1
-  fi
-  
-  # Create container
-	docker volume #todo
-  # Start the container
-	docker run #todo
-  # Make sure you understand what's `$?`
-	exit $?
-	;;
+    # Check the number of CLI arguments
+    if [ $# -ne 3 ]; then
+      echo "Create requires username and password"
+      exit 1
+    fi
 
-  start|stop) 
-  # Check instance status; exit 1 if container has not been created
-  if [ $container_status # ... todo
-  # ... todo
+    # Create a Docker volume for persistent data storage
+    docker volume create jrvs-psql-data
 
-  # Start or stop the container
-	docker container $cmd jrvs-psql
-	exit $?
-	;;	
-  
+    # Create and run the PostgreSQL container
+    docker run --name jrvs-psql \
+      -e POSTGRES_USER=$db_username \
+      -e POSTGRES_PASSWORD=$db_password \
+      -d \
+      -v jrvs-psql-data:/var/lib/postgresql/data \
+      -p 5432:5432 \
+      postgres:latest
+
+    # Check if the container was created successfully
+    exit $?
+    ;;
+
+  start|stop)
+    # Check if the container has been created
+    if [ $container_status -ne 0 ]; then
+      echo "Container has not been created"
+      exit 1
+    fi
+
+    # Start or stop the container based on the command
+    docker container $cmd jrvs-psql
+    exit $?
+    ;;	
+
   *)
-	echo 'Illegal command'
-	echo 'Commands: start|stop|create'
-	exit 1
-	;;
-esac 
-
-
-
+    # Handle invalid commands
+    echo "Illegal command"
+    echo "Commands: start|stop|create"
+    exit 1
+    ;;
+esac
 
