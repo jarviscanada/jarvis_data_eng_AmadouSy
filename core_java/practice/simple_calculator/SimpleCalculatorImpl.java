69b60ae8c52ec41364cfc8ec19a3241a34107a23
Amadou Sy <amadou9550@gmail.com>
2025-02-06 15:41:53 +0000

    Junit Exercise

---
+++ core_java/practice/simple_calculator/SimpleCalculatorImpl.java
@@ -0,0 +1,39 @@
+public class SimpleCalculatorImpl implements SimpleCalculator {
+
+    @Override
+    public int add(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int subtract(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int multiply(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double divide(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int power(int x, int y) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double abs(double x) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
